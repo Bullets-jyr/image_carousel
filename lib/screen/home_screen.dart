280c6a10394635c83636b839146f61a72b4f28f8
Bullets-jyr <jyr@bullets.co.kr>
2023-04-16 11:05:01 +0900

    Timer 사용해보기

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -8,17 +10,38 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  Timer? timer;
+
+  @override
+  void initState() {
+    super.initState();
+
+    timer = Timer.periodic(Duration(seconds: 1), (timer) {
+      print('Timer!');
+    });
+  }
+
+  @override
+  void dispose() {
+    if (timer != null) {
+      timer!.cancel();
+    }
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: PageView(
         // map: 원하는 값으로 변경할 수 있다.
-        children: [1, 2, 3, 4, 5].map(
-          (e) => Image.asset(
-            'asset/img/image_$e.jpeg',
-            fit: BoxFit.cover,
-          ),
-        ).toList(),
+        children: [1, 2, 3, 4, 5]
+            .map(
+              (e) => Image.asset(
+                'asset/img/image_$e.jpeg',
+                fit: BoxFit.cover,
+              ),
+            )
+            .toList(),
       ),
     );
   }
