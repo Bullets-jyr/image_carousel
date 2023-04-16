643a9f8c15bf7639b077eb3dcb7d3515cd2b0956
Bullets-jyr <jyr@bullets.co.kr>
2023-04-16 11:35:48 +0900

    PageController 사용해보기

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -11,18 +12,34 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   Timer? timer;
+  PageController pageController = PageController(
+    initialPage: 0,
+  );
 
   @override
   void initState() {
     super.initState();
 
-    timer = Timer.periodic(Duration(seconds: 1), (timer) {
+    timer = Timer.periodic(Duration(seconds: 4), (timer) {
       print('Timer!');
+      int currentPage = pageController.page!.toInt();
+      int nextPage = currentPage + 1;
+
+      if (nextPage > 4) {
+        nextPage = 0;
+      }
+
+      pageController.animateToPage(
+        nextPage,
+        duration: Duration(milliseconds: 400),
+        curve: Curves.linear,
+      );
     });
   }
 
   @override
   void dispose() {
+    pageController.dispose();
     if (timer != null) {
       timer!.cancel();
     }
@@ -31,8 +48,15 @@ class _HomeScreenState extends State<HomeScreen> {
 
   @override
   Widget build(BuildContext context) {
+    // 상태바 색상 변경 가능
+    SystemChrome.setSystemUIOverlayStyle(
+      SystemUiOverlayStyle.light,
+      // SystemUiOverlayStyle.dark,
+    );
+
     return Scaffold(
       body: PageView(
+        controller: pageController,
         // map: 원하는 값으로 변경할 수 있다.
         children: [1, 2, 3, 4, 5]
             .map(
